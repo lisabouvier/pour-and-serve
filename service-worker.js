c79191af6f2e0772580a2f04963880cf90acf2aa
Lisa <lisabouvier@gmail.com>
2026-04-02 20:09:51 -0400

    first commit

---
+++ service-worker.js
@@ -0,0 +1,41 @@
+const CACHE_NAME = "pour-and-serve-v3";
+const ASSETS = [
+  "/",
+  "/index.html",
+  "/style.css",
+  "/manifest.webmanifest",
+  "/icons/icon-192.png",
+  "/icons/icon-512.png"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys
+          .filter((key) => key !== CACHE_NAME)
+          .map((key) => caches.delete(key))
+      )
+    )
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  if (event.request.url.includes("drinks.json")) {
+    // always fetch fresh drinks
+    event.respondWith(fetch(event.request));
+  } else {
+    // cache everything else
+    event.respondWith(
+      caches.match(event.request).then((cachedResponse) => {
+        return cachedResponse || fetch(event.request);
+      })
+    );
+  }
+});
